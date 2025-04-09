fd4a03df4a9fb47af1b89045fe89355c28a052b3
Oleg Kravtsov <msi-oleg@outlook.com>
2025-04-09 13:25:24 +0700

    feat(lab2.2): create project

---
+++ lab2.2/src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <stdint.h>
+
+// Настройки подключения
+#define DHT11_PIN         D7  // GPIO13
+#define FAN_PIN           D8  // GPIO15
+#define SWITCH_PIN        D3  // GPIO0
+#define LIGHT_SENSOR_PIN  A0  // ADC
+#define RED_LED_PIN       D0  // GPIO16
+#define GREEN_LED_PIN     D1  // GPIO5
+#define BLUE_LED_PIN      D2  // GPIO4
+#define I2C_SDA_PIN       D6  // GPIO12
+#define I2C_SCL_PIN       D5  // GPIO14
+
+void setup()
+{
+  
+}
+
+void loop()
+{
+
+}
